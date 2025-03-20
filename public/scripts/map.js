ffc662545016ee0e7146d3ff7badb9e8b533c407
frostie <mattwiner04@gmail.com>
2025-03-19 19:27:00 -0700

    VP42

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -4,8 +4,8 @@ document.addEventListener("DOMContentLoaded", async () => {
     const map = L.map("map").setView([0, 0], 2); // Default view
 
     // Load OpenStreetMap tiles
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: '&copy; OpenStreetMap contributors'
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://carto.com/">CARTO</a>'
     }).addTo(map);
 
     async function loadPOIs() {
